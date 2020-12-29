80ca9c82f2790948c2057e83a8384abbbc33cd55
Ahmed Hassan <Ahmed99dodo1999@gmail.com>
2020-12-28 20:54:01 -0500

    added tailwindcss file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
